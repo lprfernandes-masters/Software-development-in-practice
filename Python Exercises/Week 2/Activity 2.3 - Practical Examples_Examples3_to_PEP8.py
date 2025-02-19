1dc270f1c379a327d62dfb0dbca52a1f12941f12
LUIS FERNANDES <lprfernandes@gmail.com>
2025-02-19 12:00:24 +0100

    changed folder structure and naming of the files

---
+++ Python Exercises/Week 2/Activity 2.3 - Practical Examples_Examples3_to_PEP8.py
@@ -0,0 +1,31 @@
+# Rewrite the examples above to PEP Standards.
+# https://holypython.com/beginner-python-exercises/exercise-18-python-operators/
+
+
+# Exercise 18-a: Assignment Operator =
+# Let's start with the most basic . Assign a list of colors to the variable: ["yellow", "white", "blue"]
+
+lst = ["yellow", "white", "blue"]
+
+print(lst)
+
+
+# Exercise 18-b: Division Operator /
+# Assign division of a to b to the variable.
+
+a = 10
+b = 3
+
+result = a / b
+
+print(result)
+
+
+# Exercise 18-c: Addition using +=
+# Add 100 to the variable using +=
+
+vertical_speed = 750
+vertical_speed += 100
+
+
+print(vertical_speed)
